75577bdb068388a53e225d66d2804da603661776
zakaria <zakaria.elmajdoubi@uit.ac.ma>
2023-11-16 11:25:44 -0800

    shell

--- main.c
+++ main.c
@@ -1,44 +1,28 @@
 #include "shell.h"
 
+
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
  *
- * Return: 0 on success, 1 on error
+ * @argc: number of arguments given to the program
+ * @argv: arguments list
+ *
+ * Return: returns the value of the last executed command
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	char *cmdline = NULL;
+	char **cmdargs;
+	char prompt[] = "(hsh) ";
+	Bool status = true;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+	name = (argv[0] != NULL) ? argv[0] : NULL;
 
-	if (ac == 2)
+	while (status)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
+		write(1, prompt, strlen(prompt));
+		cmdline = get_user_input();
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
-}
+
+	return (0);
+}}
